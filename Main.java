f13d312bfb3bb723bc41782130de8f84343b90a8
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2023-05-02 20:12:17 -0300

    Desenvolvendo uma tabuada para o Projeto Mentoria

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Bem vindo(a)");
+		System.out.println("Vamos realizar uma tabuada?");
+		System.out.println("Qual o nome do aluno?");
+		
+
+	}
+
+}
