b26bda65f67efaaccf606358d48cc4cb13a62b04
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2023-05-03 21:06:04 -0300

    Adicionando o imput do usuário

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Main {
 
@@ -6,6 +7,14 @@ public class Main {
 		System.out.print("Bem vindo(a)");
 		System.out.println("Vamos realizar uma tabuada?");
 		System.out.println("Qual o nome do aluno?");
+		
+		Scanner sc = new Scanner (System.in);
+		
+		String nome;
+		nome = sc.next();
+		System.out.println(nome);
+		
+				
 		
 
 	}
