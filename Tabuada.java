92ab1c8192d75c44a640f0d0a20cc1632970f564
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2023-05-07 14:24:50 -0300

    Adicionando funcionalidades ao projeto

---
+++ Tabuada.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Tabuada {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Bem vindo(a)");
+		System.out.println("Vamos realizar uma tabuada?");
+		
+		int valor;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Digite o valor da tabuada:");
+		valor = teclado.nextInt();
+		for(int i=0; i<=10; i++ ) {
+			System.out.println(valor + "x" + i);
+			
+		
+				
+		
+
+	}
+
+}
+}
